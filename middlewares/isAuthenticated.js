8987d1e79f4821b4778eadc07b8785d0a5c2d2ed
kanishk tiwari <kanishk.swe@gmail.com>
2024-08-13 17:09:59 +0530

    add isAuthenticated middleware

---
+++ middlewares/isAuthenticated.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+const isAuthenticated = async(req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if(!token) {
+            return res.status(401).json({
+                message: 'User not authenticated!',
+                success: false
+            });
+        }
+        const verifiedUser = jwt.verify(token, process.env.SECRET_KEY);
+        if(!verifiedUser){
+            return res.status(401).json({
+                message: 'Invalid',
+                success: false
+            });
+        }
+        req.id = verifiedUser.userId;
+        next();
+    } catch (err) {
+        console.log(err)
+    }
+}
